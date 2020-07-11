d342f3fabb4bed661eb28eb2fa34a6677a3eb911
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-07-11 15:52:27 +0200

    exercises is done

--- src/strategies/StdInStrategy.java
+++ src/strategies/StdInStrategy.java
@@ -8,19 +8,23 @@ import java.util.Scanner;
  * Created by amen on 8/17/17.
  */
 public class StdInStrategy implements IInputStrategy {
-
+private Scanner scanner;
     public StdInStrategy() {
+        scanner =  new Scanner(System.in);
     }
 
     @Override
     public int getInt() {
+        return scanner.nextInt();
     }
 
     @Override
     public String getString() {
+       return scanner.nextLine();
     }
 
     @Override
     public double getDouble() {
+        return scanner.nextDouble();
     }
 }
